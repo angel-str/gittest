73544a1d348910d3a99cf93fcdb24d265141cdf0
Angel <Angel@Angel-Dator>
2020-12-10 12:34:06 +0100

    Länk2

--- GitHubTest/src/prog1övningar/Övn9_7.java
+++ GitHubTest/src/prog1övningar/Övn9_7.java
@@ -1,13 +1,11 @@
 package prog1övningar;
-//https://go.codetogether.com/#/iWYj6qPzDpMjLT2FEqmb4f/IfHfxtaTMmaBckiy8g3OpN
+//https://go.codetogether.com/#/iWYj6qPzDpMjLT2FEqmb4f/AsLN4k9BmsJP1nfnXxNsfc
 import java.util.stream.IntStream;
 
 public class Övn9_7 {
 	public static void main(String[] args) {
 		
-		System.out.println(
-				isPrime(3)
-				);
+		System.out.println(isPrime(3));
 		
 	}
 	static boolean isPrime(int num) {
