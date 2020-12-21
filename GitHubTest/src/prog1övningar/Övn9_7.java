99829c74696796a5a54a25832b42c8cf5e4f7054
angel-str <60510865+angel-str@users.noreply.github.com>
2020-12-21 13:06:19 +0100

    Primtal

--- GitHubTest/src/prog1övningar/Övn9_7.java
+++ GitHubTest/src/prog1övningar/Övn9_7.java
@@ -1,4 +1,5 @@
 package prog1övningar;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 //https://go.codetogether.com/#/iWYj6qPzDpMjLT2FEqmb4f/2rr4fO2BG7lDtQl9PEVOXW
@@ -6,12 +7,27 @@ import java.util.stream.IntStream;
 public class Övn9_7 {
 	public static void main(String[] args) {
 		
-		System.out.println(isPrime(3));
+		System.out.println("Skriv in två tal som blir ditt intervall:");
+		Scanner input = new Scanner(System.in);
+		int startTal=input.nextInt();
+		int slutTal=input.nextInt();
+		System.out.println();
+		
+		testPrimes(startTal, slutTal);
 		
 	}
 	static boolean isPrime(int num) {
 		return num>1 && IntStream
 				.range(2, num)
 				.noneMatch(index -> num % index==0);
+	}
+	
+	static void testPrimes(int a, int b) {
+		
+		IntStream
+		.range(a, b)
+		.filter(e-> isPrime(e))
+		.forEach(e-> System.out.println(e));
+		
 	}
 }
